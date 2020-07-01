ce8028d77754220f3c93200346e5112567dffb9f
Mumtaz Irteqa Ahmed <fareed2000ahmed@gmail.com>
2020-07-01 21:04:41 +0530

    Updates

---
+++ precache-manifest.2aa97ee62ca0a43e6cdbccc37a33e0da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1ef3b2c0b999f32d11b413ced892aa4",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "de97c611d2ea06fd400f",
+    "url": "/monsters-rolodex/static/css/main.ab1ba301.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de97c611d2ea06fd400f",
+    "url": "/monsters-rolodex/static/js/main.84726445.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
